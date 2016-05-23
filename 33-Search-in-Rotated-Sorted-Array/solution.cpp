89be5d8ac201277e616a1f3ac3f05f14430dfdf1
pmdiano <pmdiano@gmail.com>
2016-05-22 20:12:02 -0700

    Default commit comment

--- 33-Search-in-Rotated-Sorted-Array/solution.cpp
+++ 33-Search-in-Rotated-Sorted-Array/solution.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        if (nums.empty())
+            return -1;
+
         int start = 0;
         int end = nums.size() - 1;
         int mid;
@@ -9,16 +12,18 @@ public:
             mid = start + (end - start) / 2;
             if (nums[mid] == target)
                 return mid;
-            else if (nums[mid] < target) {
-                if (nums[mid] <= nums[end] && nums[end] < target)
-                    end = mid - 1;
-                else
+            else if (nums[mid] < nums[end]) {
+                // 从mid到end一定是有序的
+                if (nums[mid] < target && target <= nums[end])
                     start = mid + 1;
+                else
+                    end = mid - 1;
             } else {
-                if (target <= nums[end] && nums[end] < nums[mid])
-                    start = mid + 1;
-                else
+                // 从start到mid一定是有序的
+                if (nums[start] <= target && target < nums[mid])
                     end = mid - 1;
+                else
+                    start = mid + 1;
             }
         }
 
