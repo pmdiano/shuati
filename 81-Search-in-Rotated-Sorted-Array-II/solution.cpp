f0f1bc0b4a9e1b4245d1d6a4b57c86e34f9b5417
pmdiano <pmdiano@gmail.com>
2016-05-22 20:28:42 -0700

    Default commit comment

---
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        if (nums.empty())
+            return false;
+
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return true;
+            else if (nums[mid] < nums[end]) {
+                if (nums[mid] < target && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            } else if (nums[mid] > nums[end]) {
+                if (nums[start] <= target && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            } else {
+                --end;
+            }
+        }
+
+        return false;
+    }
+};
