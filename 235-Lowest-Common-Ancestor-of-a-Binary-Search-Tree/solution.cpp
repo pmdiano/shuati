55ea772f65cae4d8caf5999a21ef9c00ff533ed1
pmdiano <pmdiano@gmail.com>
2016-06-11 19:59:55 -0700

    Default commit comment

---
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || !p || !q) {
+            return nullptr;
+        }
+
+        while (root) {
+            if ((p->val <= root->val && q->val >= root->val) || (p->val >= root->val && q->val <= root->val)) {
+                return root;
+            }
+            if (p->val <= root->val && q->val <= root->val) {
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+        }
+
+        return nullptr;
+    }
+};
