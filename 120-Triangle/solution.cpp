560b024741f8afc24c31e81685a028230e82aef2
pmdiano <pmdiano@gmail.com>
2016-06-19 15:45:16 -0700

    Default commit comment

--- 120-Triangle/solution.cpp
+++ 120-Triangle/solution.cpp
@@ -6,22 +6,22 @@ public:
             return 0;
         }
 
-        vector<vector<int>> f(n, vector<int>(n, 0));
-        f[0][0] = triangle[0][0];
+        vector<int> f(n, 0);
+        f[0] = triangle[0][0];
 
         for (int i = 1; i < n; i++) {
-            f[i][0] = f[i-1][0] + triangle[i][0];
-            f[i][i] = f[i-1][i-1] + triangle[i][i];
-            for (int j = 1; j < i; j++) {
-                f[i][j] = min(f[i-1][j-1], f[i-1][j]) + triangle[i][j];
+            f[i] = f[i-1] + triangle[i][i];
+            for (int j = i-1; j >= 1; j--) {
+                f[j] = min(f[j], f[j-1]) + triangle[i][j];
             }
+            f[0] = f[0] + triangle[i][0];
         }
 
-        int result = f[n-1][0];
+        int ans = f[0];
         for (int j = 1; j < n; j++) {
-            result = min(result, f[n-1][j]);
+            ans = min(ans, f[j]);
         }
 
-        return result;
+        return ans;
     }
 };
