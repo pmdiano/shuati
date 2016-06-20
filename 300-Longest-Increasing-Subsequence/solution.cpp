4b42fc18e15381d139a73c1a01b7187631c2bc13
pmdiano <pmdiano@gmail.com>
2016-06-19 22:40:44 -0700

    Default commit comment

--- 300-Longest-Increasing-Subsequence/solution.cpp
+++ 300-Longest-Increasing-Subsequence/solution.cpp
@@ -1,28 +1,32 @@
 class Solution {
+    int findFirstGreaterThan(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1, mid = 0;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return start;
+    }
+
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
-        }
+        vector<int> LIS;
 
-        int n = nums.size();
-        vector<int> f(n, 1);
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i] && f[j] + 1 > f[i]) {
-                    f[i] = f[j] + 1;
-                }
+        for (int i = 0; i < nums.size(); i++) {
+            if (LIS.empty() || LIS[LIS.size() - 1] < nums[i]) {
+                LIS.push_back(nums[i]);
+            } else {
+                int pos = findFirstGreaterThan(LIS, nums[i]);
+                LIS[pos] = nums[i];
             }
         }
 
-        int ans = f[0];
-        for (int i = 1; i < n; i++) {
-            if (f[i] > ans) {
-                ans = f[i];
-            }
-        }
-
-        return ans;
+        return LIS.size();
     }
 };
