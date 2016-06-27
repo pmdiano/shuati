6037a37d56770aa48a8e0663cee25eca6a18096c
pmdiano <pmdiano@gmail.com>
2016-06-26 21:32:23 -0700

    Default commit comment

---
+++ 147-Insertion-Sort-List/solution2.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+
+        while (head) {
+            ListNode* next = head->next;
+            ListNode* prev = dummy;
+
+            // This is not stable
+            while (prev->next && prev->next->val < head->val) {
+                prev = prev->next;
+            }
+            head->next = prev->next;
+            prev->next = head;
+            head = next;
+        }
+
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
