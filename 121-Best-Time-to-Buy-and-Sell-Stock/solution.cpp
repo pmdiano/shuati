d2889f74edd54beade314dfdf5c67bbc2d0c2d28
pmdiano <pmdiano@gmail.com>
2016-07-02 07:48:14 -0700

    Default commit comment

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
-        int price = numeric_limits<int>::max();
-        for (int i = 0; i < prices.size(); i++) {
-            if (i > 0) {
-                profit = max(profit, prices[i] - price);
-            }
+        int price = prices.empty() ? numeric_limits<int>::max() : prices[0];
+
+        for (int i = 1; i < prices.size(); i++) {
+            profit = max(profit, prices[i] - price);
             price = min(price, prices[i]);
         }
+
         return profit;
     }
 };
