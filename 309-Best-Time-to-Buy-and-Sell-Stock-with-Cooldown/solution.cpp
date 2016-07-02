dcec99f659f0158904571d23250bacc6b0534ed0
pmdiano <pmdiano@gmail.com>
2016-07-02 10:05:28 -0700

    Default commit comment

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.cpp
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.cpp
@@ -5,13 +5,16 @@ public:
             return 0;
         }
 
+        // local[i] means the maximum possible profit from day 0~i, with a guaranteed sell on day i
+        // global[i] means the maximum possible profit from day 0~i
         vector<int> local(prices.size(), 0);
         vector<int> global(prices.size(), 0);
 
         for (int i = 1; i < prices.size(); i++) {
             int diff = prices[i] - prices[i-1];
             local[i] = max(local[i-1] + diff, diff);
-            if (i>=3) {
+            if (i>3) {
+                // With cool down, sell on day i, buy on day i-1, cooldown on day i-2, sell on day i-3 (or earlier)
                 local[i] = max(local[i], global[i-3] + diff);
             }
 
