37c39c2065a1ee5d6803d1fa72100f593e8a88ba
pmdiano <pmdiano@gmail.com>
2016-07-05 07:57:15 -0700

    Auto commit message

--- 146-LRU-Cache/solution.cpp
+++ 146-LRU-Cache/solution.cpp
@@ -1,65 +1,86 @@
 class LRUCache{
     struct ListNode {
         int key;
-        int val;
+        int value;
         ListNode* next;
-        ListNode(int k, int v) : key(k), val(v), next(nullptr) {}
+        ListNode(int k, int v) : key(k), value(v), next(nullptr) {}
     };
 
-    unordered_map<int, ListNode*> hash;
-    ListNode *head;
-    ListNode *tail;
+    ListNode* head;
+    ListNode* tail;
+    unordered_map<int, ListNode*> prevHash;
     int capacity;
 
     void move_to_tail(int key) {
-        ListNode* prev = hash[key];
+        ListNode* prev = prevHash[key];
         ListNode* node = prev->next;
-
-        if (node != tail) {
-            prev->next = node->next;
-            if (node->next) {
-                hash[node->next->key] = prev;
-            }
-
-            tail->next = node;
-            node->next = nullptr;
-    
-            hash[key] = tail;
-            tail = node;
+        if (node == tail) {
+            // Already at tail, nothing to do
+            return;
         }
+
+        // Take out node, need to update node's next's hash
+        prevHash[node->next->key] = prev;
+        prev->next = node->next;
+
+        // Insert node to tail, update hash, update tail
+        prevHash[node->key] = tail;
+        tail->next = node;
+        node->next = nullptr;
+        tail = node;
+    }
+
+    void insert_at_tail(int key, int value) {
+        ListNode* node = new ListNode(key, value);
+        prevHash[key] = tail;
+
+        tail->next = node;
+        tail = node;
     }
 
     void remove_head_next() {
-        ListNode* node = head->next;
-
-        if (tail == node) {
+        if (head->next == tail) {
+            // Only one node, reset everything
+            prevHash.clear();
+            delete tail;
             tail = head;
+            return;
         }
 
-        if (node) {
-            hash.erase(node->key);
-            head->next = node->next;
-            if (head->next) {
-                hash[head->next->key] = head;
-            }
-            delete node;
-        }
+        ListNode* node = head->next;
+        ListNode* next = node->next;
+
+        // Remove node
+        prevHash.erase(node->key);
+        delete node;
+        head->next = next;
+
+        // Update next node's hash
+        prevHash[next->key] = head;
     }
 
 public:
     LRUCache(int capacity) {
-        this->head = new ListNode(0, 0);
-        this->tail = head;
         this->capacity = capacity;
+        this->head = new ListNode(0, 0);
+        this->tail = this->head;
     }
-    
+
+    ~LRUCache() {
+        while (head) {
+            ListNode* node = head;
+            head = head->next;
+            delete node;
+        }
+    }
+
     int get(int key) {
-        if (hash.find(key) == hash.end()) {
+        if (prevHash.find(key) == prevHash.end()) {
             return -1;
         }
 
         move_to_tail(key);
-        return hash[key]->next->val;
+        return prevHash[key]->next->value;
     }
     
     void set(int key, int value) {
@@ -67,18 +88,16 @@ public:
             return;
         }
 
-        if (hash.find(key) == hash.end()) {
-            if (hash.size() == capacity) {
-                remove_head_next();
-            }
-            ListNode* node = new ListNode(key, value);
-            tail->next = node;
-            hash[key] = tail;
-            tail = node;
+        if (prevHash.find(key) != prevHash.end()) {
+            move_to_tail(key);
+            prevHash[key]->next->value = value;
             return;
         }
 
-        move_to_tail(key);
-        tail->val = value;
+        if (prevHash.size() == capacity) {
+            remove_head_next();
+        }
+
+        insert_at_tail(key, value);
     }
 };
