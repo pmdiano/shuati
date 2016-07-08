5e70b4c242735129096705a85d1340cdfd3182e7
pmdiano <pmdiano@gmail.com>
2016-07-07 21:40:05 -0700

    Default commit comment

--- 254-Factor-Combinations/solution.cpp
+++ 254-Factor-Combinations/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
-    void dfs(int n, int last, vector<int>& current, vector<vector<int>>& ans) {
-        for (int i = 2; i < n; i++) {
-            if (n / i >= i && i >= last && n % i == 0) {
-                current.push_back(i);
-                current.push_back(n/i);
-                ans.push_back(current);
-                current.pop_back();
-                dfs(n/i, i, current, ans);
-                current.pop_back();
+    void dfs(int n, int start, vector<int>& current, vector<vector<int>>& ans) {
+        for (int i = start; i <= (int)sqrt(n); i++) {
+            if (n % i != 0) {
+                continue;
             }
+
+            current.push_back(i);
+            current.push_back(n/i);
+            ans.push_back(current);
+
+            current.pop_back();
+            dfs(n/i, i, current, ans);
+            current.pop_back();
         }
     }
 
@@ -20,7 +23,7 @@ public:
             return ans;
         }
 
-        dfs(n, 0, current, ans);
+        dfs(n, 2, current, ans);
         return ans;
     }
 };
