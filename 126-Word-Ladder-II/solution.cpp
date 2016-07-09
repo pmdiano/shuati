16a632fd4ad8f18f14508cdb10ee2584e897c1a4
pmdiano <pmdiano@gmail.com>
2016-07-09 10:50:59 -0700

    Default commit comment

--- 126-Word-Ladder-II/solution.cpp
+++ 126-Word-Ladder-II/solution.cpp
@@ -1,87 +1,92 @@
 class Solution {
-    void getPath(string startWord, unordered_map<string, vector<string>>& precursor, vector<vector<string>>& result, vector<string>& current) {
-        if (current.back() == startWord) {
-            vector<string> path = current;
-            reverse(path.begin(), path.end());
-            result.push_back(path);
+    void dfs(string beginWord, int steps, unordered_map<string, unordered_set<string>>& precursor, vector<string>& path, vector<vector<string>>& ans) {
+        if (path.back() == beginWord) {
+            vector<string> path2 = path;
+            reverse(path2.begin(), path2.end());
+            ans.push_back(path2);
             return;
         }
 
-        vector<string>& next = precursor[current.back()];
-        for (int i = 0; i < next.size(); i++) {
-            current.push_back(next[i]);
-            getPath(startWord, precursor, result, current);
-            current.pop_back();
+        if (path.size() == steps) {
+            return;
+        }
+
+        string word = path.back();
+        for (string prev : precursor[word]) {
+            path.push_back(prev);
+            dfs(beginWord, steps, precursor, path, ans);
+            path.pop_back();
         }
     }
 
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, unordered_set<string> &wordList) {
-        vector<vector<string>> result;
+        vector<vector<string>> ans;
         if (beginWord == endWord) {
             vector<string> path;
             path.push_back(beginWord);
-            result.push_back(path);
-            return result;
+            ans.push_back(path);
+            return ans;
         }
 
-        int n = beginWord.size();
-        if (n == 0 || endWord.size() != beginWord.size()) {
-            return result;
+        if (beginWord.length() != endWord.length()) {
+            return ans;
         }
 
-        unordered_map<string, vector<string> > precursor;
-        queue<string> Q;
-        Q.push(beginWord);
+        unordered_map<string, unordered_set<string>> precursor;
+        queue<string> q;
+        int steps = 0;
+        bool found = false;
+
+        q.push(beginWord);
         wordList.erase(beginWord);
         wordList.insert(endWord);
 
-        bool found = false;
-        while (!found && !Q.empty()) {
-            int Qsize = Q.size();
+        while (!q.empty() && !found) {
+            steps++;
             unordered_set<string> thisLayer;
             unordered_set<string> nextLayer;
 
-            for (; Qsize; Qsize--) {
-                string word = Q.front();
-                Q.pop();
-                thisLayer.insert(word);
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                thisLayer.insert(q.front());
+                q.pop();
             }
 
-            for (unordered_set<string>::const_iterator it = thisLayer.begin(); it != thisLayer.end(); it++) {
-                string word = *it;
-                string prev = *it;
+            for (string word : thisLayer) {
                 if (word == endWord) {
                     found = true;
                     break;
+                    // OK to break here, since we already got all precursors of endWord in previous rounds
                 }
-                
-                for (int i = 0; i < n; i++) {
-                    char oldChar = word[i];
-                    for (char newChar = 'a'; newChar <= 'z'; newChar++) {
-                        if (newChar == oldChar) continue;
-                        word[i] = newChar;
-                        if (wordList.find(word) != wordList.end() && thisLayer.find(word) == thisLayer.end()) {
-                            Q.push(word);
-                            nextLayer.insert(word);
-                            precursor[word].push_back(prev);
+
+                string next = word;
+                for (int i = 0; i < next.size(); i++) {
+                    char original = next[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == original) continue;
+                        next[i] = c;
+                        if (wordList.find(next) != wordList.end()) {
+                            q.push(next);
+                            nextLayer.insert(next);
+                            precursor[next].insert(word);   // Key of this problem
                         }
                     }
-                    word[i] = oldChar;
+                    next[i] = original;
                 }
             }
 
-            for (unordered_set<string>::const_iterator it = nextLayer.begin(); it != nextLayer.end(); it++) {
-                wordList.erase(*it);
+            for (string word : nextLayer) {
+                wordList.erase(word);
             }
         }
 
         if (found) {
-            vector<string> current;
-            current.push_back(endWord);
-            getPath(beginWord, precursor, result, current);
+            vector<string> path;
+            path.push_back(endWord);
+            dfs(beginWord, steps, precursor, path, ans);
         }
 
-        return result;
+        return ans;
     }
 };
