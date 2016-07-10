3c2641b4353d1b656a1b1e43504099227fd22dc9
pmdiano <pmdiano@gmail.com>
2016-07-10 14:44:56 -0700

    Default commit comment

---
+++ 13-Roman-to-Integer/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        if (s.empty()) {
+            return 0;
+        }
+
+        unordered_map<char, int> num;
+        num['I'] = 1;
+        num['V'] = 5;
+        num['X'] = 10;
+        num['L'] = 50;
+        num['C'] = 100;
+        num['D'] = 500;
+        num['M'] = 1000;
+
+        int ans = num[s[s.length() - 1]];
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (num[s[i]] < num[s[i+1]]) {
+                ans -= num[s[i]];
+            } else {
+                ans += num[s[i]];
+            }
+        }
+
+        return ans;
+    }
+};
