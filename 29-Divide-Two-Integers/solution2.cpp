e82ad5d0218530f5cc22238fa5edc6082fcb2288
pmdiano <pmdiano@gmail.com>
2016-07-11 18:39:57 -0700

    Auto commit message

---
+++ 29-Divide-Two-Integers/solution2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long long a = dividend >= 0 ? dividend : -(long long)dividend;
+        long long b = divisor >= 0 ? divisor : -(long long)divisor;
+        long long result = 0, shift = 31;
+        while (shift >= 0) {
+            if (a >= (b << shift)) {
+                a -= (b << shift);
+                result += (1LL << shift);
+            }
+            shift--;
+        }
+
+        result = (dividend ^ divisor) >> 31 ? -result : result;
+        if (result > INT_MAX) {
+            return INT_MAX;
+        }
+        return (int)result;
+    }
+};
