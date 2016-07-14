68e8b291c80a676634ea2bb106ac9a0130bdcb58
Qirong Ma <pmdiano@gmail.com>
2016-07-14 07:50:08 -0700

    rename

--- 067-Add-Binary/solution.cpp
+++ 067-Add-Binary/solution.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        return "";
+        if (a.length() < b.length()) {
+            swap(a, b);
+        }
+
+        int n = a.length(), m = b.length();
+        string result(n, '0');
+        int sum = 0, carry = 0;
+        int i = n-1, j = m-1;
+
+        for (; j >= 0; i--, j--) {
+            sum = (a[i] - '0') + (b[j] - '0') + carry;
+            carry = sum / 2;
+            result[i] = sum % 2 + '0';
+        }
+
+        for (; i >= 0; i--) {
+            sum = (a[i] - '0') + carry;
+            carry = sum / 2;
+            result[i] = sum % 2 + '0';
+        }
+
+        if (carry) {
+            result = "1" + result;
+        }
+
+        return result;
     }
 };
--- 069-Sqrt(x)/solution.cpp
+++ 069-Sqrt(x)/solution.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
+        if (x <= 0) {
+            return 0;
+        }
+
+        int lo = 1, hi = x;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int quo = x / mid;
+
+            if (quo == mid) {
+                return mid;
+            } else if (quo < mid) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return (int)hi;
     }
 };
