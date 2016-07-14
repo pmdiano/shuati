b67e8c487e6a1c82bcdd232968aa41709efef51b
pmdiano <pmdiano@gmail.com>
2016-07-14 09:18:18 -0700

    Auto commit message

--- 73-Set-Matrix-Zeroes/solution.cpp
+++ 73-Set-Matrix-Zeroes/solution.cpp
@@ -1,6 +1,54 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
+        if (matrix.empty() || matrix[0].empty()) {
+            return;
+        }
+
+        bool row0 = false, col0 = false;
+        int m = matrix.size(), n = matrix[0].size(), i, j;
+
+        for (j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+
+        for (i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                col0 = true;
+                break;
+            }
+        }
+
+        for (i = 1; i < m; i++) {
+            for (j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (i = 1; i < m; i++) {
+            for (j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (row0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (col0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
