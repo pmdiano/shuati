01a60c12cffba6c6a213d194207f033a7b748122
pmdiano <pmdiano@gmail.com>
2016-07-17 11:35:34 -0700

    Default commit comment

--- 151-Reverse-Words-in-a-String/solution.cpp
+++ 151-Reverse-Words-in-a-String/solution.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     void reverseWords(string &s) {
-        
+        string ans;
+        const char* pch = strtok(const_cast<char*>(s.c_str()), " ");
+        while (pch) {
+            if (ans.length()) ans = " " + ans;
+            ans = string(pch) + ans;
+            pch = strtok(NULL, " ");
+        }
+        s = ans;
     }
 };
