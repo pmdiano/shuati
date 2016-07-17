17f05858afdd0117c45c94d8531012d08e437289
pmdiano <pmdiano@gmail.com>
2016-07-17 14:12:42 -0700

    Default commit comment

--- 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.cpp
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
-        
+        int i = 0, j = 0, count = 0, ans = 0;
+        int window[256] = {0};
+        while (i < s.length()) {
+            if (window[s[i]] || count < k) {
+                if (window[s[i++]]++ == 0) {
+                    count++;
+                }
+                if (ans < i-j) {
+                    ans = i-j;
+                }
+            } else {
+                if (--window[s[j++]] == 0) {
+                    count--;
+                }
+            }
+        }
+        return ans;
     }
 };
