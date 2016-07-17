fbd03caca0232955920ba2aae8a859170ad3b748
pmdiano <pmdiano@gmail.com>
2016-07-16 21:54:55 -0700

    Default commit comment

--- 137-Single-Number-II/solution.cpp
+++ 137-Single-Number-II/solution.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for (int mask = 1; mask; mask <<= 1) {
-            int bit = 0;
-            for (int& x : nums) {
-                if (x & mask) bit++;
-            }
-            if (bit % 3) {
-                ans |= mask;
-            }
+        int one = 0, two = 0;
+        for (int x : nums) {
+            two |= (one & x);
+            one ^= x;
+            int three = two & one;
+            two &= ~three;
+            one &= ~three;
         }
-        return ans;
+        return one;
     }
 };
