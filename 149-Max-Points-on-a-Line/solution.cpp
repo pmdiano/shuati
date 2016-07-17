827f3586e0476dc950aad55123511f891f6c1c67
pmdiano <pmdiano@gmail.com>
2016-07-17 11:01:09 -0700

    Default commit comment

--- 149-Max-Points-on-a-Line/solution.cpp
+++ 149-Max-Points-on-a-Line/solution.cpp
@@ -1,91 +1,115 @@
+/**
+ * Definition for a point.
+ * struct Point {
+ *     int x;
+ *     int y;
+ *     Point() : x(0), y(0) {}
+ *     Point(int a, int b) : x(a), y(b) {}
+ * };
+ */
 class Solution {
-    struct Line {
-        int k_denom;
-        int k_num;
-        int y0_denom;
-        int y0_num;
-        Line() : k_denom(0), k_num(0), y0_denom(0), y0_num(0) {}
-        bool operator<(const Line& l2) const {
-            if (k_denom != l2.k_denom)
-                return k_denom < l2.k_denom;
-            if (k_num != l2.k_num)
-                return k_num < l2.k_num;
-            if (y0_denom != l2.y0_denom)
-                return y0_denom < l2.y0_denom;
-            return y0_num < l2.y0_num;
-        }
-        bool pass_point(Point& p) {
-        	if (k_denom == 0)
-        		return p.x == y0_denom;
-        	if (k_num == 0)
-        		return p.y == y0_num;
-        	return p.y*k_denom == (p.x*k_num + y0_num);
-        }
-    };
-    
-    int gcd(int a, int b) {
-        // Assume both a and b are positive integers
+    static int gcd(int a, int b) {
         if (a < 0) a = -a;
         if (b < 0) b = -b;
-        int tmp;
         while (a % b) {
-            tmp = b;
-            b = a % b;
-            a = tmp;
+            int tmp = a % b;
+            a = b;
+            b = tmp;
         }
         return b;
     }
-    
-    Line get_line(Point& p1, Point& p2) {
-        Line line;
-        if (p1.x == p2.x) {
-            line.k_num = 1;
-            line.k_denom = 0;
-            line.y0_num = 0;
-            line.y0_denom = p1.x;
+
+    static pair<int, int> normalize(pair<int, int> p) {
+        // p.second is guaranteed to be non-zero when calling this
+        if (p.first == 0) {
+            return make_pair(0, 1);
         }
-        else if (p1.y == p2.y) {
-            line.k_num = 0;
-            line.k_denom = 1;
-            line.y0_num = p1.y;
-            line.y0_denom = 0;
+        int cd = gcd(p.first, p.second);
+        p.first /= cd;
+        p.second /= cd;
+        if (p.first < 0) {
+            p.first = -p.first;
+            p.second = -p.second;
         }
-        else {
-            int g = gcd(p2.y - p1.y, p2.x - p1.x);
-            line.k_denom = (p2.x - p1.x) / g;
-            line.k_num = (p2.y - p1.y) / g;
-            if (line.k_denom < 0) {
-                line.k_denom = - line.k_denom;
-                line.k_num = - line.k_num;
-            }
-            line.y0_num = line.k_denom * p2.y - line.k_num * p2.x;
-            line.y0_denom = line.k_denom;
-        }
-        return line;
+        return p;
     }
-    
+
+    struct Line {
+        // ax + by + c = 0
+        int a;
+        int b;
+        int c;
+        int d;
+        Line() : a(0), b(0), c(0) {}
+        Line(int aa, int bb, int cc, int dd) : a(aa), b(bb), c(cc) {}
+        Line (Point& p1, Point& p2) {
+            if (p1.x == p2.x) {
+                a = 1;
+                b = 0;
+                c = -p1.x;
+                return;
+            }
+            if (p1.y == p2.y) {
+                a = 0;
+                b = 1;
+                c = -p1.y;
+                return;
+            }
+
+            pair<int, int> k = Solution::normalize(make_pair(p2.y-p1.y, p2.x-p1.x));
+            pair<int, int> d = Solution::normalize(make_pair(p2.x*p1.y-p1.x*p2.y, p2.x-p1.x));
+
+            int cd = Solution::gcd(k.second, d.second);
+            a = k.first * d.second / cd;
+            b = -k.second * d.second / cd;
+            c = k.second * d.first / cd;
+            if (a < 0) {
+                a = -a;
+                b = -b;
+                c = -c;
+            }
+        }
+
+        bool operator==(const Line& rhs) const {
+            return (a == rhs.a && b == rhs.b && c == rhs.c);
+        }
+
+        bool containsPoint(const Point& p) const {
+            return a * p.x + b * p.y + c == 0;
+        }
+    };
+
+    struct LineHasher {
+        std::size_t operator()(const Solution::Line& l) const {
+            return (hash<int>()(l.a) ^ (hash<int>()(l.b) << 1) >> 1) ^ (hash<int>()(l.c) << 1);
+        }
+    };
+
 public:
     int maxPoints(vector<Point>& points) {
-        if (points.size() < 3)
+        if (points.size() <= 1) {
             return points.size();
+        }
 
-        int nLine = 0;
+        int n = 0;
         Line l;
-        std::map<Line, int> hough;
-        for (int i = 0; i < points.size(); i++)
-            for (int j = i+1; j < points.size(); j++) {
-                Line line = get_line(points[i], points[j]);
-                int tmp = ++hough[line];
-                if (tmp > nLine) {
-                    nLine = tmp;
+        unordered_map<Line, int, LineHasher> hash;
+        for (int i = 1; i < points.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                Line line(points[i], points[j]);
+                if (++hash[line] > n) {
+                    n = hash[line];
                     l = line;
                 }
             }
+        }
 
         int ans = 0;
-        for (int i = 0; i < points.size(); i++)
-        	if (l.pass_point(points[i]))
-        		ans++;
+        for (Point& p : points) {
+            if (l.containsPoint(p)) {
+                ans++;
+            }
+        }
         return ans;
     }
 };
