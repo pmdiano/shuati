ab397cd1bc70d8cfc3fb8dc442d120f9920569de
pmdiano <pmdiano@gmail.com>
2016-07-18 08:44:46 -0700

    Default commit comment

--- 165-Compare-Version-Numbers/solution.cpp
+++ 165-Compare-Version-Numbers/solution.cpp
@@ -5,14 +5,20 @@ public:
         while (i1 < version1.length() || i2 < version2.length()) {
             if (i1 < version1.length()) {
                 while (version1[i1] && version1[i1] != '.') i1++;
-                v1 = atoi(const_cast<char*>(version1.substr(j1, i1-j1).c_str()));
+                char c = version1[i1];
+                version1[i1] = '\0';
+                v1 = atoi(const_cast<char*>(version1.c_str()) + j1);
+                version1[i1] = c;
             } else {
                 v1 = 0;
             }
 
             if (i2 < version2.length()) {
                 while (version2[i2] && version2[i2] != '.') i2++;
-                v2 = atoi(const_cast<char*>(version2.substr(j2, i2-j2).c_str()));
+                char c = version2[i2];
+                version2[i2] = '\0';
+                v2 = atoi(const_cast<char*>(version2.c_str()) + j2);
+                version2[i2] = c;
             } else {
                 v2 = 0;
             }
