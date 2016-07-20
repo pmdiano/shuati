067fa15441d34375d9165e4503b535888c736415
pmdiano <pmdiano@gmail.com>
2016-07-20 07:30:42 -0700

    Auto commit message

---
+++ 201-Bitwise-AND-of-Numbers-Range/solution2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int ans = 0;
+        for (int mask = 1; mask && mask <= n; mask <<= 1) {
+            if (n-m+1 <= mask && m & mask && n & mask) {
+                ans |= mask;
+            }
+        }
+        return ans;
+    }
+};
