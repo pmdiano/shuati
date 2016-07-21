7a3d6a0113c72985fd486c65eae846c137d23c2e
pmdiano <pmdiano@gmail.com>
2016-07-20 19:28:48 -0700

    Default commit comment

--- 204-Count-Primes/solution.cpp
+++ 204-Count-Primes/solution.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int countPrimes(int n) {
         if (n <= 1) return 0;
-        vector<int> isPrime(n, 1);
+        vector<bool> isPrime(n, true);
 
-        for (int i = 2; i * i < n; i++) {
+        for (int i = 2; i < sqrt((float)n); i++) {
             if (!isPrime[i]) continue;
             for (int j = i * i; j < n; j+=i) {
-                isPrime[j] = 0;
+                isPrime[j] = false;
             }
         }
 
