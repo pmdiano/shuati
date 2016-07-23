c5453ee26f3859896d9dc39eeb2f45dd64536488
pmdiano <pmdiano@gmail.com>
2016-07-22 21:32:23 -0700

    Default commit comment

---
+++ 224-Basic-Calculator/solution.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    char precede(char theta1, char theta2) {
+        if (theta1 == '#')
+            return theta2 == '#' ? '=' : '<';
+        else if (theta1 == '(')
+            return theta2 == ')' ? '=' : '<';
+        else
+            return theta2 == '(' ? '<' : '>';
+    }
+
+    int operate(int a, char theta, int b) {
+        switch (theta) {
+            case '+': return a+b;
+            case '-': return a-b;
+            default: return 0;
+        }
+    }
+public:
+    int calculate(string s) {
+        stack<char> optr;
+        stack<int> opnd;
+        optr.push('#');
+        s.push_back('#');
+
+        int i = 0;
+        while (i < s.length()) {
+            if (s[i] == ' ') { i++; continue; }
+            if (isdigit(s[i])) {
+                int num = 0;
+                while (isdigit(s[i])) num = 10 * num + s[i++] - '0';
+                opnd.push(num);
+                continue;
+            }
+            switch(precede(optr.top(), s[i])) {
+                case '<':
+                    optr.push(s[i++]);
+                    break;
+                case '=':
+                    optr.pop();
+                    i++;
+                    break;
+                case '>':
+                    char theta = optr.top(); optr.pop();
+                    int b = opnd.top(); opnd.pop();
+                    int a = opnd.top(); opnd.pop();
+                    int c = operate(a, theta, b);
+                    opnd.push(c);
+                    break;
+            }
+        }
+
+        return opnd.top();
+    }
+};
