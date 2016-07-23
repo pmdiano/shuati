0625520a963a8940855825a62ba7f7b835df1158
pmdiano <pmdiano@gmail.com>
2016-07-22 19:44:20 -0700

    Default commit comment

---
+++ 207-Course-Schedule/solution2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool dfs(vector<vector<int>>& edges, int i, vector<int>& visited) {
+        if (visited[i] == -1) return false;
+        if (visited[i] == 1) return true;
+
+        visited[i] = -1;
+        for (int neighbor : edges[i]) {
+            if (!dfs(edges, neighbor, visited)) {
+                return false;
+            }
+        }
+        visited[i] = 1;
+        return true;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        if (numCourses <= 0) return false;
+        int n = numCourses;
+        vector<vector<int>> edges(n);
+        vector<int> visited(n, 0);
+
+        for (auto& p : prerequisites) {
+            edges[p.first].push_back(p.second);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!dfs(edges, i, visited)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
