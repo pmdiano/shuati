6fe9c2767c34fc6abee9292d73ce27e9e01fdfb2
pmdiano <pmdiano@gmail.com>
2016-07-26 18:00:35 -0700

    Default commit comment

---
+++ 251-Flatten-2D-Vector/solution.cpp
@@ -0,0 +1,19 @@
+class Vector2D {
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+    }
+
+    int next() {
+        
+    }
+
+    bool hasNext() {
+        
+    }
+};
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i(vec2d);
+ * while (i.hasNext()) cout << i.next();
+ */
