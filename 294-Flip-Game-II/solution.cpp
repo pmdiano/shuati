7b2b17baa142d286fe51f2be8e32df96f941f610
pmdiano <pmdiano@gmail.com>
2016-07-31 17:41:31 -0700

    Default commit comment

--- 294-Flip-Game-II/solution.cpp
+++ 294-Flip-Game-II/solution.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool canWin(string s) {
-        for (int i = 0; i < (int)s.length() - 1; i++) {
-            if (s[i] == '+' && s[i+1] == '+') {
-                string t = s;
-                t[i] = '-', t[i+1] = '-';
-                if (!canWin(t)) return true;
+        for (int i = -1; (i = s.find("++", i+1)) != string::npos; ) {
+            if (!canWin(s.substr(0, i) + "-" + s.substr(i+2))) {
+                return true;
             }
         }
         return false;
