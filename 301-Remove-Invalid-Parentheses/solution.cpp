63e41165c168a7e43cd2d3a7200494aedda0641b
pmdiano <pmdiano@gmail.com>
2016-08-18 08:04:42 -0700

    Default commit comment

--- 301-Remove-Invalid-Parentheses/solution.cpp
+++ 301-Remove-Invalid-Parentheses/solution.cpp
@@ -1,54 +1,41 @@
 class Solution {
-    int maxLen;
-    set<string> myset;
-
-    void search(string &s, int pos, string current, int left, int maxLeft) {
-        if (pos == s.length()) {
-            if (left == 0 && current.length() != 0) {
-                if (maxLeft > maxLen) {
-                    maxLen = maxLeft;
-                }
-                if (maxLeft == maxLen) {
-                    myset.insert(current);
-                }
-            }
-            return;
+    bool isValid(string& str) {
+        int left = 0;
+        for (char c : str) {
+            if (c == '(') left++;
+            else if (c == ')' && left-- == 0) return false;
         }
-
-        if ((int)s.length() - pos < left) {
-            // Can never match parentheses
-            return;
-        }
-        if (maxLeft + ((int)s.length() - pos - left) / 2 < maxLen) {
-            // Cannot be at least maxLen
-            return;
-        }
-
-        if (s[pos] == '(') {
-            search(s, pos+1, current+"(", left+1, maxLeft+1);
-            search(s, pos+1, current, left, maxLeft);
-        } else if (s[pos] == ')') {
-            if (left > 0) {
-                search(s, pos+1, current+")", left-1, maxLeft);
-            }
-            search(s, pos+1, current, left, maxLeft);
-        } else {
-            search(s, pos+1, current+string(1, s[pos]), left, maxLeft);
-        }
+        return left == 0;
     }
 
 public:
     vector<string> removeInvalidParentheses(string s) {
-        myset = set<string>();
-        maxLen = 0;
+        vector<string> res;
+        queue<string> q;
+        unordered_set<string> visited;
+        bool found = false;
+        q.push(s);
+        visited.insert(s);
 
-        string current;
-        search(s, 0, current, 0, 0);
+        while (!q.empty()) {
+            s = q.front();
+            q.pop();
+            if (isValid(s)) {
+                res.push_back(s);
+                found = true;
+            }
+            if (found) continue;
 
-        vector<string> ans(myset.begin(), myset.end());
-        if (ans.empty()) {
-            ans.push_back("");
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] != '(' && s[i] != ')') continue;
+                string t = s.substr(0, i) + s.substr(i+1);
+                if (visited.find(t) == visited.end()) {
+                    q.push(t);
+                    visited.insert(t);
+                }
+            }
         }
-        return ans;
+
+        return res;
     }
 };
