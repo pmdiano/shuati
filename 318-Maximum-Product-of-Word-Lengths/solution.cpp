b2d07aaac8392d4c323031535401e04bf990b8ca
pmdiano <pmdiano@gmail.com>
2016-09-01 20:59:53 -0700

    Default commit comment

--- 318-Maximum-Product-of-Word-Lengths/solution.cpp
+++ 318-Maximum-Product-of-Word-Lengths/solution.cpp
@@ -5,19 +5,15 @@ public:
             return 0;
         }
 
-        vector<int> lens(words.size(), 0), bits(words.size(), 0);
+        int res = 0;
+        vector<int> bits(words.size(), 0);
         for (int i = 0; i < words.size(); i++) {
-            lens[i] = (int)words[i].length();
             for (char c : words[i]) {
                 bits[i] |= (1 << (c - 'a'));
             }
-        }
-
-        int res = 0;
-        for (int i = 0; i < words.size(); i++) {
-            for (int j = 0; j < words.size(); j++) {
-                if ((bits[i] & bits[j]) == 0 && lens[i] * lens[j] > res) {
-                    res = lens[i] * lens[j];
+            for (int j = 0; j < i; j++) {
+                if ((bits[i] & bits[j]) == 0 && words[i].length() * words[j].length() > res) {
+                    res = words[i].length() * words[j].length();
                 }
             }
         }
