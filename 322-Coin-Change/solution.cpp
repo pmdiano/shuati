a6ebf49e66ff9dde89f3a46f009ef77650de6dd7
pmdiano <pmdiano@gmail.com>
2016-09-02 18:34:30 -0700

    Auto commit message

--- 322-Coin-Change/solution.cpp
+++ 322-Coin-Change/solution.cpp
@@ -4,17 +4,17 @@ public:
         if (amount < 0) return -1;
 
         int n = coins.size();
-        vector<int> dp(amount+1, INT_MAX);
+        vector<int> dp(amount+1, amount+1);
         dp[0] = 0;
 
         for (int total = 1; total <= amount; total++) {
             for (int i = 0; i < n; i++) {
-                if (coins[i] <= total && dp[total-coins[i]] != INT_MAX) {
+                if (coins[i] <= total) {
                     dp[total] = min(dp[total], dp[total-coins[i]] + 1);
                 }
             }
         }
 
-        return dp[amount] != INT_MAX ? dp[amount] : -1;
+        return dp[amount] <= amount ? dp[amount] : -1;
     }
 };
