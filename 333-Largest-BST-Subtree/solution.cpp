827dc5ebc5be5c232b78469fd68784ae6512bfba
pmdiano <pmdiano@gmail.com>
2016-09-03 20:14:27 -0700

    Default commit comment

---
+++ 333-Largest-BST-Subtree/solution.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    struct Result {
+        bool isBst;
+        int minVal;
+        int maxVal;
+        int maxBstNodes;
+    };
+
+    Result helper(TreeNode* root) {
+        Result res;
+        if (!root) {
+            res.isBst = true;
+            res.maxBstNodes = 0;
+            return res;
+        }
+
+        Result left = helper(root->left);
+        Result right = helper(root->right);
+
+        if (left.isBst && right.isBst) {
+            if ((!root->left || left.maxVal < root->val) &&
+                (!root->right || right.minVal > root->val)) {
+                res.isBst = true;
+                res.minVal = root->left ? left.minVal : root->val;
+                res.maxVal = root->right ? right.maxVal : root->val;
+                res.maxBstNodes = left.maxBstNodes + right.maxBstNodes + 1;
+                return res;
+            }
+        }
+
+        res.isBst = false;
+        res.maxBstNodes = max(left.maxBstNodes, right.maxBstNodes);
+        return res;
+    }
+
+public:
+    int largestBSTSubtree(TreeNode* root) {
+        return helper(root).maxBstNodes;
+    }
+};
