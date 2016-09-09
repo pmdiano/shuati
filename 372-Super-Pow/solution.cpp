98f63b7999b6d1316bb425e1e41452511d3d2906
pmdiano <pmdiano@gmail.com>
2016-09-09 13:28:10 -0700

    Auto commit message

--- 372-Super-Pow/solution.cpp
+++ 372-Super-Pow/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
-    long long myPow(int a, int n) {
-        long long res = 1;
+    int myPow(int a, int n) {
+        int res = 1;
         while (n) {
             if (n & 1) res = (res * a) % 1337;
             a = (a * a) % 1337;
@@ -8,9 +8,10 @@ class Solution {
         }
         return res % 1337;
     }
+
 public:
     int superPow(int a, vector<int>& b) {
-        long long res = 1;
+        int res = 1;
         a %= 1337;
         for (int i = 0; i < b.size(); i++) {
             res = myPow(res, 10) * myPow(a, b[i]) % 1337;
