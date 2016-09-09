f78a0ce3187097408454f648d60ee7569f28c05e
pmdiano <pmdiano@gmail.com>
2016-09-09 15:51:51 -0700

    Auto commit message

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.cpp
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.cpp
@@ -10,11 +10,13 @@ class Solution {
 public:
     vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         priority_queue<pair<int, int>, vector<pair<int, int>>, pair_comp<int, int>> heap;
-        for (int n1 : nums1) {
-            for (int n2 : nums2) {
-                heap.push(make_pair(n1, n2));
-                if (heap.size() > k) {
+        for (int i1 = 0; i1 < min(k, (int)nums1.size()); i1++) {
+            for (int i2 = 0; i2 < min(k, (int)nums2.size()); i2++) {
+                if (heap.size() < k) {
+                    heap.push(make_pair(nums1[i1], nums2[i2]));
+                } else if (nums1[i1] + nums2[i2] < heap.top().first + heap.top().second) {
                     heap.pop();
+                    heap.push(make_pair(nums1[i1], nums2[i2]));
                 }
             }
         }
