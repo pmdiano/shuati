71dcd1bf4a8d58a939811746db25da501e65f0ce
pmdiano <pmdiano@gmail.com>
2016-09-09 20:31:56 -0700

    Default commit comment

--- 375-Guess-Number-Higher-or-Lower-II/solution2.cpp
+++ 375-Guess-Number-Higher-or-Lower-II/solution2.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int getMoneyAmount(int n) {
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        vector<vector<int>> dp(n+2, vector<int>(n+1, 0));
         for (int len = 2; len <= n; len++) {
             for (int start=1, end=start+len-1; end <= n; start++, end++) {
                 dp[start][end] = INT_MAX;
                 for (int guess = start; guess <= end; guess++) {
-                    int cost = guess + max(dp[start][guess-1], guess==n ? 0 : dp[guess+1][end]);
-                    if (cost < dp[start][end]) {
-                        dp[start][end] = cost;
-                    }
+                    int cost = guess + max(dp[start][guess-1], dp[guess+1][end]);
+                    dp[start][end] = min(dp[start][end], cost);
                 }
             }
         }
