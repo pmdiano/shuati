19f9d1077e436dd43abba779e983d990467be57c
pmdiano <pmdiano@gmail.com>
2016-09-13 21:07:36 -0700

    Default commit comment

---
+++ 384-Shuffle-an-Array/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    vector<int> original;
+public:
+    Solution(vector<int> nums) : original(nums) {
+        
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return original;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int> shuffle = original;
+        for (int i = shuffle.size()-1, len = shuffle.size(); i >= 0; i--) {
+            int j = rand() % len;
+            swap(shuffle[i], shuffle[j]);
+        }
+        return shuffle;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * vector<int> param_1 = obj.reset();
+ * vector<int> param_2 = obj.shuffle();
+ */
