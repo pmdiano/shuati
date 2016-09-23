1161608b1b949af3cdae49c813b65d7050dfe7b5
pmdiano <pmdiano@gmail.com>
2016-09-22 19:10:21 -0700

    Default commit comment

---
+++ 388-Longest-Absolute-File-Path/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        int res = 0, i = 0;
+        stack<int> stk;
+        stk.push(0);
+        input.push_back('\n');
+
+        while (input[i]) {
+            int depth = 0;
+            while (input[i] == '\t') { depth++; i++; }
+
+            bool has_dot = false;
+            int j = i;
+            while (input[i] != '\n') {
+                if (input[i++] == '.') has_dot = true;
+            }
+
+            while (depth < (int)stk.size()-1) stk.pop();
+            if (has_dot) {
+                res = max(res, stk.top()+i-j);
+            } else {
+                stk.push(stk.top()+i-j+1);
+            }
+
+            i++;
+        }
+
+        return res;
+    }
+};
