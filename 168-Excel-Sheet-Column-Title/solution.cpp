9e53eaa622b0be14ff10c4c01942dd6cb353f06c
pmdiano <pmdiano@gmail.com>
2016-10-08 21:40:29 -0700

    Default commit comment

--- 168-Excel-Sheet-Column-Title/solution.cpp
+++ 168-Excel-Sheet-Column-Title/solution.cpp
@@ -3,7 +3,7 @@ public:
     string convertToTitle(int n) {
         string ans;
         while (n) {
-            ans.insert(0, 1, 'A' + (--n)%26);
+            ans.insert(0, 1, 'A' + (--n) % 26);
             n /= 26;
         }
         return ans;
